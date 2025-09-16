7fea0f9e5df9b040354c54ecec4d6a2128f45c72
ServenErnestSerquiña <ServenErnestSerquia@SMU-0198>
2025-09-16 18:09:24 +0800

    Add project files.

---
+++ Serquiña.SE_User_Input_Validator/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class NegativeAgeException
+    Inherits ApplicationException
+    Public Sub New(message As String)
+        MyBase.New(message)
+    End Sub
+End Class
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim input As String = TextBox1.Text
+            Dim age As Integer = Integer.Parse(input)
+
+            ' Check if age is negative
+            If age < 0 Then
+                Throw New NegativeAgeException("Age cannot be negative.")
+            End If
+
+            MessageBox.Show("Your age is: " & age.ToString())
+
+        Catch ex As FormatException
+            MessageBox.Show("Please enter a valid number for age.")
+
+        Catch ex As NegativeAgeException
+            MessageBox.Show(ex.Message)
+
+        End Try
+    End Sub
+End Class
---
+++ Serquiña.SE_User_Input_Validator/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Serquiña.SE_User_Input_Validator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Serquiña.SE_User_Input_Validator")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("47c4d0f5-7179-45ae-b55c-b29edfff1c80")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
